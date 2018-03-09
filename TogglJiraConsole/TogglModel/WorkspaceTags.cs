5472d171d7367f66b45bd2ac64978339483838f6
Marcelo Gonçalves <marcelolimagoncalves@gmail.com>
2018-03-09 13:12:35 -0300

    alteracoes

---
+++ TogglJiraConsole/TogglModel/WorkspaceTags.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TogglJiraConsole.TogglModel
+{
+    public class WorkspaceTags
+    {
+        public int id { get; set; }
+        public int wid { get; set; }
+        public string name { get; set; }
+    }
+}
