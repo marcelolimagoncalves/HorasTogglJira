71097c2c65a744021268ae7ad64cebeadd773315
Marcelo de Lima Goncalves <marcelolimagoncalves@gmail.com>
2018-03-19 22:26:03 -0300

    alter

---
+++ TogglJiraConsole/JiraModel/DbJiraContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TogglJiraConsole.JiraModel
+{
+    public class DbJiraContext
+    {
+        public void ConsultarWorkLogs()
+        {
+            using (var connection = new MySql.Data.MySqlClient.MySqlConnection("Server=localhost;Database=world;Uid=root;Pwd=1qazxsw2@;"))
+            {
+                connection.Open();
+                using (var command = new MySql.Data.MySqlClient.MySqlCommand("SELECT * FROM city", connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+    }
+}
